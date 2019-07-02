3092fa9997067ee7dc3077edf05b36d0dfd4f488
Fabricio <fabriciopassos3916@gmail.com>
2019-07-02 18:53:20 -0300

    Função menu criada

---
+++ calculadora.py
@@ -0,0 +1,35 @@
+class Calculadora():
+    def __init__(self):
+        self.menu()
+
+    def menu(self):
+        opcao = int(input("1-Somar\n2-Subtrair\n3-Multiplicar\n4-Dividir\nEntre com qualquer outro numero para fechar\n: "))
+        if(opcao == 1):
+            self.somar()
+        elif(opcao == 2):
+            self.subtrair()
+        elif(opcao == 3):
+            self.multiplicar
+        elif(opcao == 4):
+            self.dividir()
+        else:
+            print("Encerrando...")
+    def somar(self):
+        print('x')
+        self.menu()
+    def subtrair(self):
+        print('x')
+        self.menu()
+    def multiplicar(self):
+        print('x')
+        self.menu()        
+    def dividir(self):
+        print('x')
+        self.menu()
+
+calculadora1 = Calculadora()
+
+
+    
+        
+        
